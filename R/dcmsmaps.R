12be77b5b6fa3f865f69727f288ddbe1806a65d9
DCMSstats <evidence@culture.gov.uk>
2017-01-17 10:34:17 +0000

    Add files via upload

--- R/dcmsmaps.R
+++ R/dcmsmaps.R
@@ -13,7 +13,7 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-dcmsmaps <- function(csvfile='',outfile='',mincol='',maxcol='',scale=TRUE,england=FALSE,labels=TRUE,pound=FALSE) {
+dcmsmaps <- function(csvfile='',outfile='',mincol='',maxcol='',scale=TRUE,england=FALSE,englandwales=FALSE,labels=TRUE,pound=FALSE) {
 
   # Read in files
   data(NUTS1)
@@ -26,6 +26,12 @@ dcmsmaps <- function(csvfile='',outfile='',mincol='',maxcol='',scale=TRUE,englan
   # Subset if England only
   if(england==TRUE){
     area2 <-  area2[area2$NATION=='England',]
+  }
+
+  # Subset if England and Wales only
+  if(englandwales==TRUE){
+    area2 <-  area2[area2$NATION!='Scotland',]
+    area2 <-  area2[area2$NATION!='Northern Ireland',]
   }
 
   # Set pound sign if the data is financial
@@ -67,7 +73,7 @@ dcmsmaps <- function(csvfile='',outfile='',mincol='',maxcol='',scale=TRUE,englan
   # Make Key
   if(scale==TRUE){
 
-    if(england==FALSE){
+    if(england==FALSE){# || englandwales==FALSE){
 
       x=c(600000,700000,700000,600000)
       y=c(620000,620000,630000,630000)
@@ -84,7 +90,7 @@ dcmsmaps <- function(csvfile='',outfile='',mincol='',maxcol='',scale=TRUE,englan
       text(710000,840000, paste0(pounds,prettyNum(max(area2@data$mapdata),big.mark=',')),adj=0,cex=fsize,col="black")
     }
 
-    if(england==TRUE){
+    if(england==TRUE){ #|| englandwales==TRUE){
       x=c(50000,150000,150000,50000)
       y=c(420000,420000,430000,430000)
 
@@ -137,59 +143,66 @@ dcmsmaps <- function(csvfile='',outfile='',mincol='',maxcol='',scale=TRUE,englan
     #North East
     polygon(c(470000,670000),c(440000,440000),border=maincol)
     text(675000,440000,
-         paste0(area2@data$NAME[4],"\n",pounds,format(area2@data$mapdata[4],nsmall=1)),
+         paste0(area2@data$NAME[4],"\n",pounds,formatC(area2@data$mapdata[4],big.mark=',',digits=digitsl,format="f")),
          adj=0,cex=fsize,col="black")
 
     #North West
     polygon(c(370000,670000),c(510000,510000),border=maincol)
     text(675000,510000,
-         paste0(area2@data$NAME[5],"\n",pounds,format(area2@data$mapdata[5],nsmall=1)),
+         paste0(area2@data$NAME[5],"\n",pounds,formatC(area2@data$mapdata[5],big.mark=',',digits=digitsl,format="f")),
          adj=0,cex=fsize,col="black")
 
     #South East
     polygon(c(500000,500000),c(130000,50000),border=maincol)
     text(500000,20000,
-         paste0(area2@data$NAME[6],"\n",pounds,format(area2@data$mapdata[6],nsmall=1)),
+         paste0(area2@data$NAME[6],"\n",pounds,formatC(area2@data$mapdata[6],big.mark=',',digits=digitsl,format="f")),
          adj=0,cex=fsize,col="black")
 
     #South West
     polygon(c(140000,290000),c(115000,115000),border=maincol)
     text(135000,115000,
-         paste0(area2@data$NAME[7],"\n",pounds,format(area2@data$mapdata[7],nsmall=1)),
+         paste0(area2@data$NAME[7],"\n",pounds,formatC(area2@data$mapdata[7],big.mark=',',digits=digitsl,format="f")),
          adj=1,cex=fsize,col="black")
 
     #West Midlands
     polygon(c(140000,380000),c(310000,310000),border=maincol)
     text(135000,310000,
-         paste0(area2@data$NAME[8],"\n",pounds,format(area2@data$mapdata[8],nsmall=1)),
+         paste0(area2@data$NAME[8],"\n",pounds,formatC(area2@data$mapdata[8],big.mark=',',digits=digitsl,format="f")),
          adj=1,cex=fsize,col="black")
 
     #Yorkshire & The Humber
     polygon(c(410000,670000),c(580000,580000),border=maincol)
     text(675000,580000,
-         paste0(area2@data$NAME[9],"\n",pounds,format(area2@data$mapdata[9],nsmall=1)),
+         paste0(area2@data$NAME[9],"\n",pounds,formatC(area2@data$mapdata[9],big.mark=',',digits=digitsl,format="f")),
          adj=0,cex=fsize,col="black")
 
-    if(england==FALSE){
+    if(england==FALSE){ #|| englandwales==FALSE){
       #Wales
       polygon(c(140000,290000),c(220000,220000),border=maincol)
       text(135000,220000,
-           paste0(area2@data$NAME[12],"\n",pounds,format(area2@data$mapdata[12],nsmall=1)),
+           paste0(area2@data$NAME[12],"\n",pounds,formatC(area2@data$mapdata[12],big.mark=',',digits=digitsl,format="f")),
            adj=1,cex=fsize,col="black")
 
       #Scotland
       polygon(c(250000,670000),c(900000,900000),border=maincol)
       text(675000,900000,
-           paste0(area2@data$NAME[11],"\n",pounds,format(area2@data$mapdata[11],nsmall=1)),
+           paste0(area2@data$NAME[11],"\n",pounds,formatC(area2@data$mapdata[11],big.mark=',',digits=digitsl,format="f")),
            adj=0,cex=fsize,col="black")
 
       #Northern Ireland
       polygon(c(100000,100000),c(500000,420000),border=maincol)
       text(95000,415000,
-           paste0(area2@data$NAME[10],"\n",pounds,format(area2@data$mapdata[10],nsmall=1)),
-           adj=1,cex=fsize,col="black")
+           paste0(area2@data$NAME[10],"\n",pounds,formatC(area2@data$mapdata[10],big.mark=',',digits=digitsl,format="f")),
+           adj=0,cex=fsize,col="black")
     }
 
+    #if(englandwales==TRUE){
+      #Wales
+    #  polygon(c(140000,290000),c(220000,220000),border=maincol)
+      #text(135000,220000,
+      #     paste0(area2@data[area2$NATION=='Wales',]$NAME,"\n",pounds,format(area2@data[area2$NAME=='Wales',]$mapdata,nsmall=1)),
+      #     adj=1,cex=fsize,col="black")
+    #}
   }
 
   dev.off()
